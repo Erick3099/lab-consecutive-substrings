ce21afd55bc2a839c730061cfd896472cb7fd1cd
erick <ericwambua5150@gmail.com>
2025-12-30 09:03:01 +0300

    add changes

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,37 @@
-function consecutiveSubstrings(string) {
-  // type your code here
-}
+ 
+  // func takes 1 para (string)
+  // return all consecutive....
+    // empty array
+    // loop controls starting pos of each substring
+    // x start of a substring
+    // starts at index 0(outer loop)
+    // innner loop ending pos of the substring
+    // 
 
+function consecutiveSubstrings(string) {
+  const result = []; 
+  for (let x = 0; x < string.length; x++) { 
+    // let temp =""
+    //  for (let y = x; y < string.length; y++) {
+    //   temp += string[y];
+    //   result.push(temp);
+    for (let y = x + 1; y <= string.length; y++) {
+      result.push(string.substring(x, y));
+    }
+  }
+  return result;
+}
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
+  console.log("in return: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
   console.log("");
 
-  console.log("Expecting: ['a']");
+  console.log("in return: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
 }
 
 module.exports = consecutiveSubstrings;
+
 
 
